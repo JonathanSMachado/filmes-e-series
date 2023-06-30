d923a75a319da60f981285476c0467d224adeda3
Jonathan S. Machado <jonathansmachado@gmail.com>
2023-06-29 23:18:03 -0300

    update logo

--- app/components/Logo/index.tsx
+++ app/components/Logo/index.tsx
@@ -4,11 +4,9 @@ export default function Logo() {
   return (
     <Link to="/">
       <img
-        src="/images/logo.png"
+        src="/images/filmes_series.png"
         alt="Filmes e Séries"
-        className="w-64 h-auto outline-none hover:scale-105 focus:scale-105 transition-transform ease-in-out delay-1000"
-        width="256"
-        height="110"
+        className="w-44 h-auto mt-4 outline-none hover:scale-105 focus:scale-105 transition-transform ease-in-out"
       />
     </Link>
   );
