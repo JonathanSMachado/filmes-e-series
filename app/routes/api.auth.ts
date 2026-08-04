161db0805a74fb2a46803d0a5e60487a210e4eeb
Jonathan S. Machado <jonathansmachado@gmail.com>
2026-08-03 23:48:22 -0300

    Alteração para sempre exibir a tela de confirmação de conta do Google

--- app/routes/api.auth.ts
+++ app/routes/api.auth.ts
@@ -5,14 +5,19 @@ import { createSupabaseServerClient } from "~/utils/supabase.server";
 export async function action({ request }: ActionFunctionArgs) {
   const { supabase, headers } = createSupabaseServerClient(request);
   const formData = await request.formData();
-  const requestUrl = new URL(request.url);
+
   const redirectToPath = (formData.get("redirectTo") as string) || "/";
+  const requestUrl = new URL(request.url);
   const callbackUrl = `${requestUrl.origin}/api/auth/callback?next=${encodeURIComponent(redirectToPath)}`;
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
       redirectTo: callbackUrl,
+      queryParams: {
+        prompt: "select_account",
+        access_type: "offline",
+      },
     },
   });
 
